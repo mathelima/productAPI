cbe8d3481fccb975971bc02726e7af276d736f52
Ralph <ralph.souza@olist.com>
2021-02-05 14:07:33 +0000

    Product API view creation with index route.

---
+++ src/views/api.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from src.resources.product_resource import ProductResource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(ProductResource, '/api/product', endpoint='products')
+api.add_resource(ProductResource, '/api/product/<int:id>', endpoint='product')
+
+
+@app.route('/')
+def index():
+    return "Welcome"
+
+app.run(debug=True)
